2a2da0f16f47bc3849ecd5b00078977d41fa1797
41111111 <147721339+41111111@users.noreply.github.com>
2025-04-06 19:15:22 +0800

    Update line_bot_test_250405.py

--- line_bot_test_250405.py
+++ line_bot_test_250405.py
@@ -1,33 +1,33 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('uCHR0VdxUl3iyr0YAifLoGQHcdvMxGEEuk8ubMywuP7luPTgJJxAxpjecf0RKiv9zYMVtALaQlYzXU7JqYjfJEs/iRtuHqt2e68O6ELSJLV8LEQH4l4W99oiVRy7XZXF4YTxg0iiO6fIj0P4F7YMgwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('8fa23de32b81a5680eedd89d8f10249c')
+# 從環境變數取得 Line Bot 的 Secret 和 Token
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
 
+# LINE Webhook 接收路徑
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers.get('X-Line-Signature', '')
     body = request.get_data(as_text=True)
-
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)  # 解析收到的事件
     except InvalidSignatureError:
-        abort(400)
-
+        abort(400)  # 驗證不通過
+    # 處理每一個事件
+    for event in events:
+        # 如果是訊息事件且是文字訊息
+        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessage):
+            reply_text = f"你說了{event.message.text}"
+            # 回覆訊息給用戶
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    reply = f"你說的是：{msg}"
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply)
-    )
 
 if __name__ == "__main__":
     app.run()
